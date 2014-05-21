d67f2efff246921c72f18fbdd7db9301cb620e12
Theo Pavlakou <theo.pavlakou@gmail.com>
2014-05-21 11:25:16 +0100

    initial commit

---
+++ tst/testTPower.py
@@ -0,0 +1,32 @@
+from TPowerAlgorithm import * 
+tp = TPowerAlgorithm()
+import unittest
+
+"""
+# Test the truncateOperator() method works
+v = array([2.0, 8.0, 5.0, 7.0, 1.0])
+tp = TPowerAlgorithm()
+u = tp.truncateOperator(v, 2)
+print("--- Result of Truncate Operator --- ")
+print(u)
+"""
+# Test the getSparsePC algorithm works
+V = zeros((10, 2))
+V[0,0] = 5
+V[1,0] = 2
+V[3,0] = 20
+V[8,1] = 9
+V[9,1] = 8
+L = diag([20, 10])
+intermediate = dot(V, L)
+A = dot(intermediate, transpose(V))
+print("=== Printing A ====")
+print(A.shape)
+print(A)
+[x, f] = tp.getSparsePC(A, 2)
+print("=== Printing Results ===")
+
+print("--- Sparse Eigenvector ---")
+print(x.todense())
+print("--- Eigenvalue ---")
+print(f)
